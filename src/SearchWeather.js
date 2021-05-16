0caa417358bbd791979cf8cf4226c887a24a55be
BeliMoreta <77322967+BeliMoreta@users.noreply.github.com>
2021-05-16 16:27:14 +0200

    Delete

--- src/SearchWeather.js
+++ src/SearchWeather.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./SearchWeather.css";
-import WeatherInfo from "./WeatherInfo";
 import Button from "./Button";
 
 
--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,8 +1,7 @@
 import React from "react";
 
 
-import Button from "./Button";
-import WeatherInfo from "./WeatherInfo";
+
 import City from "./City";
 import SearchWeather from "./SearchWeather";
 
